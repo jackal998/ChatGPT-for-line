37bac7f7f21154470a36eed0b67d0b811347f1a8
EJ <ej_lin2000@yahoo.com.tw>
2023-03-15 00:00:58 +0800

    feat/deploy-gcp: add condition to access_token for test environment in openai.rb initializer file.

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,3 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai[:access_token]
+  config.access_token = Rails.env.test? ? "" : Rails.application.credentials.openai[:access_token]
 end
