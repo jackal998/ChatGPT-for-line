96ac42fd9ca81803c8d497c8660131b7fcf9b2a7
EJ (YiJie) <ej_lin2000@yahoo.com.tw>
2023-03-23 23:00:48 +0800

    feat: use docker compose and ssl on gce (#19)

---
+++ deployment/bashrc.md
@@ -0,0 +1,7 @@
+```
+alias docker-compose='docker run --rm \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$PWD:$PWD" \
+  -w="$PWD" \
+  docker-compose-gcr-local'
+```
--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /app
-RAILS_ENV=production bundle exec rake db:create
-RAILS_ENV=production bundle exec rake db:migrate
-
-bundle exec rails s -e production -b 0.0.0.0 -p 8080
